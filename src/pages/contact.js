32fca7a0313a50b3e120fd63c52ca3766a8fa900
Rohith <rohithkumar1319@gmail.com>
2020-07-15 18:22:09 +0530

    contactpege update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,34 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {graphql} from 'gatsby'
-import HeroSection from "../components/Reuseable/HeroSection"
-import Infoblock from "../components/Reuseable/Infoblock"
 import Contact from "../components/Reuseable/Contact/contact"
 
 
-const ContactPage = ({data}) => (
+const ContactPage = () => (
   <Layout>
     <SEO title="Home" />
-    <HeroSection
-    img={data.img.childImageSharp.fluid}
-      title="Contact Us"
-      subtitle=""
-      heroclass="about-background"
-    />
-    <Infoblock heading="How can we helop"/>
     <Contact />
   </Layout>
 )
 
-export const query = graphql`
-{ img: file(relativePath: { eq: "contact.jpg" }) {
-  childImageSharp {
-    fluid {
-      ...GatsbyImageSharpFluid_tracedSVG
-    }
-  }
-}
-}`
 
 export default ContactPage
